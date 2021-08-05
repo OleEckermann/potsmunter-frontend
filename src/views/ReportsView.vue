6d2942ae3937622458a50d6633641cab79001b1e
glaures <guido@laures.de>
2021-08-05 16:01:55 +0200

    POT-12 #resolve

--- src/views/ReportsView.vue
+++ src/views/ReportsView.vue
@@ -16,14 +16,14 @@
         </span>
         &nbsp;von {{ prescriptionCount }} Verordnung<span v-if="unassignedPrescriptionCount > 1">en</span>&nbsp;in
         diesem Abrechnungszeitraum
-        {{ unassignedPrescriptionCount === 1 ? 'ist' : 'sind' }} noch keiner TherapeutIn zugewiesen.
+        {{ unassignedPrescriptionCount === 1 ? 'ist' : 'sind' }} noch keiner Therapeutin zugewiesen.
       </div>
       <div v-else
            class="icon-text">
         <icon class="icon has-text-success" size="2x" icon="check-circle"/>
         Alle&nbsp;
         <span class="is-bold has-text-success">{{ assignedPrescriptionCount }}</span>
-        &nbsp;Behandlungen der Verordnungen in diesem Abrechnungszeitraum sind TherapeutInnen zugewiesen.
+        &nbsp;Behandlungen der Verordnungen in diesem Abrechnungszeitraum sind Therapeutinnen zugewiesen.
       </div>
       <div class="buttons mt-5">
         <div class="button is-info" v-if="unassignedPrescriptionCount > 0">Bearbeiten</div>
@@ -31,12 +31,35 @@
              v-if="unassignedPrescriptionCount > 0"
              @click="downloadReport"
              target="_parent">
-          Ignorieren und Berichte erstellen
+          Ignorieren und alle Berichte erstellen
         </div>
         <div class="button is-primary"
              v-if="unassignedPrescriptionCount === 0"
              @click="downloadReport">
-          Berichte erstellen
+          alle Berichte erstellen
+        </div>
+      </div>
+      <div class="buttons">
+        <div class="field">
+          <label for="therapistSelector">Nur für diese Therapeutin</label>
+          <div class="control is-flex is-grouped">
+            <input v-model="therapistQuery"
+                   class="input"
+                   type="text"
+                   list="therapistList"
+                   id="therapistSelector"
+                   v-debounce:300ms="therapistQueryUpdated"
+                   @input="handleTherapistInput"
+                   autofocus/>
+            <datalist id="therapistList">
+              <option v-for="therapist in therapistsList" :key="therapist" :value="therapist"/>
+            </datalist>
+            <button class="button is-primary ml-2"
+                    :disabled="!selectedTherapist"
+                    @click="downloadReport(selectedTherapist.number)">
+              Bericht erstellen
+            </button>
+          </div>
         </div>
       </div>
     </div>
@@ -53,26 +76,6 @@
         </table>
       </div>
     </div>
-    <!--
-    <form>
-      <div class="field">
-        <label for="therapistSelector">Report ansehen für</label>
-        <div class="control">
-          <input v-model="therapistQuery"
-                 class="input"
-                 type="text"
-                 list="therapistList"
-                 id="therapistSelector"
-                 v-debounce:300ms="therapistQueryUpdated"
-                 @input="handleTherapistInput"
-                 autofocus/>
-          <datalist id="therapistList">
-            <option v-for="therapist in therapists" :key="therapist.number" :value="therapist.name"/>
-          </datalist>
-        </div>
-      </div>
-    </form>
-    -->
   </div>
 </template>
 
@@ -87,6 +90,7 @@ export default {
       date: DateTime.now().setLocale('de'),
       reportInfo: {},
       therapistQuery: '',
+      selectedTherapist: undefined,
       therapists: []
     }
   },
@@ -111,6 +115,9 @@ export default {
     },
     prescriptionCount() {
       return this.unassignedPrescriptionCount + this.assignedPrescriptionCount
+    },
+    therapistsList() {
+      return this.therapists.map(t => therapistStr(t))
     }
   },
   watch: {
@@ -131,31 +138,38 @@ export default {
     navigate(n) {
       this.date = this.date.plus({month: n})
     },
-    downloadReport() {
+    downloadReport(therapistNumber) {
       this.startLoading('Berichte werden generiert')
-      this.$api.post(`/reports/${this.year}/${this.month}`, {}, {
+      const paramArr = therapistNumber ? [therapistNumber] : []
+      this.$api.post(`/reports/${this.year}/${this.month}`, paramArr, {
         responseType: 'blob'
+      }).then(response => {
+        console.log(response)
+        let blob
+        try {
+          blob = new Blob([response.data], {type: 'application/zip'})
+        } catch (e) {
+          let BlobBuilder = window.BlobBuilder || window.WebKitBlobBuilder ||
+              window.MozBlobBuilder || window.MSBlobBuilder
+          let builder = new BlobBuilder()
+          builder.append(response.data)
+          blob = builder.getBlob('application/zip');
+        }
+        let blobURL = window.webkitURL.createObjectURL(blob)
+        window.open(blobURL)
       })
-          .then(response => {
-            console.log(response)
-            let blob
-            try {
-              blob = new Blob([response.data], {type: 'application/zip'})
-            } catch (e) {
-              let BlobBuilder = window.BlobBuilder || window.WebKitBlobBuilder ||
-                  window.MozBlobBuilder || window.MSBlobBuilder
-              let builder = new BlobBuilder()
-              builder.append(response.data)
-              blob = builder.getBlob('application/zip');
-            }
-            let blobURL = window.webkitURL.createObjectURL(blob)
-            window.open(blobURL)
-          })
           .catch(error => this.handleError(error))
           .finally(() => this.stopLoading())
-          .then(() => this.loadReportInfo())
+          .then(() => {
+            this.loadReportInfo()
+          })
     },
     therapistQueryUpdated() {
+      if (this.therapistsList.indexOf(this.therapistQuery) >= 0
+          || this.therapistQuery.indexOf(',') >= 0
+          || this.therapistQuery.indexOf('[') >= 0
+          || this.therapistQuery.indexOf(']') >= 0)
+        return
       this.$api.get('/therapists', {
         params: {
           q: this.therapistQuery
@@ -163,15 +177,11 @@ export default {
       }).then(response => {
         this.therapists = response.data
       })
-    }
-    ,
+    },
     handleTherapistInput() {
       let therapist = this.therapists.find(t => therapistStr(t) === this.therapistQuery);
       if (therapist) {
-        this.$api.get(`/reports/${this.year}/${this.month}/${therapist.number}`)
-            .then(response => {
-              this.reportHtml = response.data.html
-            })
+        this.selectedTherapist = therapist
       }
     }
   }
